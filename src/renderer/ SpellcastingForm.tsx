636efbe6f3d16585872f072cc78a93a71c0db33e
Brian Stauff <11544657+bstauff@users.noreply.github.com>
2024-01-08 07:18:58 -0600

    spells removal

--- src/renderer/ SpellcastingForm.tsx
+++ src/renderer/ SpellcastingForm.tsx
@@ -1,9 +1,11 @@
 import { useState } from 'react';
+import { useFormContext } from 'react-hook-form';
 import AddSpellsForm from './AddSpellsForm';
 import SpellcastingDescriptionForm from './SpellcastingDescriptionForm';
 
 export default function SpellcastingForm() {
   const [useSpellCasting, setUseSpellCasting] = useState(false);
+  const { unregister } = useFormContext();
   return (
     <>
       <h1>Spellcasting</h1>
@@ -12,7 +14,14 @@ export default function SpellcastingForm() {
         <>
           <SpellcastingDescriptionForm />
           <AddSpellsForm />
-          <button type="button" onClick={() => setUseSpellCasting(false)}>
+          <button
+            type="button"
+            onClick={() => {
+              setUseSpellCasting(false);
+              unregister('spellcasting-description');
+              unregister('spells');
+            }}
+          >
             Remove Spellcasting
           </button>
         </>
